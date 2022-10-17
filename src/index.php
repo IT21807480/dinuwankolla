d1158baef7ab105401514bc1c2cc4c8d108bd2f5
IT21807480 <76590372+IT21807480@users.noreply.github.com>
2022-10-17 15:02:44 +0530

    Add files via upload

---
+++ src/index.php
@@ -0,0 +1,44 @@
+!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="styles/styles.css">
+	<script src="js/myScript.js"></script>
+</head>
+<body>
+<img class="logo" src="images/cart.png" width="200" height="200" alt="Shopping Cart Image">
+<center>
+	<h1>ABC Mobiles</h1>
+	<h3 id="header">The Online Shopping Store</h3>
+<hr id="test"/>
+</center>
+<ul class="menu">
+	<li class="menu"><a href="#">Home</a></li>
+	<li class="menu"><a href="news.html">News</a></li>
+	<li class="menu"><a href="register.html">Contact</a></li>
+	<li class="menu"><a href="about.php">About Us</a></li>
+</ul>
+<br/>
+<button><a href ="addItems.html">Add New Item</a></button>
+<br/>
+<center>
+
+<div class ="productData tbl" style ="border-style: groove; border-color: DarkBlue">
+<table border ="1" width ="100%">
+<tr>
+<th class ="col1">Item ID</th>
+<th class ="col1">Item Name</th>
+<th class ="col1">Descition</th>
+<th class ="col1">price</th>
+<th class ="col1">Quality</th>
+<th class ="col1">Item Code</th>
+<th class ="col1">Edit</th>
+<th class ="col1">Delete</th>
+
+</tr>
+
+</div>
+<h3 style="font-weight: bold; font-style: italic;">Created By : <a href="https://courseweb.sliit.lk/"> Your Name </a></h3>
+</center>
+</body>
+</html>
